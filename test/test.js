96e48feee3f69aa7b8999db84255bd8ac27bf496
ananya-yadav <46912630+ananya-yadav@users.noreply.github.com>
2020-01-30 15:39:01 -0500

    Update test.js

--- test/test.js
+++ test/test.js
@@ -12,6 +12,53 @@ let CreateBill = "";
 
 describe("User Test Route", function () {
     describe("POST /v1/user", function () {
+        
+        it("should successfully create a new user object", function (done) {
+        var payload = {
+            email_address: "unitTesting@husky.neu.edu",
+            first_name: "Ananya",
+            last_name: "Yadav",
+            password: "AnaEfron@1713!"
+        };
+        var server = app.listen();
+        request(server)
+            .post("/v1/user")
+            .send(payload)
+            .end(function (err, res) {
+                if (err) done(err);
+                newUserId = res.body.id;
+                res.should.have.status(201);
+                res.body.should.be.a("object");
+                done();
+            });
+    });
+        
+        it("should successfully create a new bill object", function (done) {
+        var payload = {
+            "vendor": "Test Bill",
+            "bill_date": "2020-01-29",
+            "due_date": "2020-01-31",
+            "amount_due": 10.22,
+            "categories": [
+              "college",
+              "tuition",
+              "spring2020"
+            ],
+            "paymentStatus": "no_payment_required"
+          };
+        var server = app.listen();
+        request(server)
+            .post("/v1/bill")
+            .auth('unitTesting@husky.neu.edu', 'AnaEfron@1713!')
+            .send(payload)
+            .end(function (err, res) {
+                if (err) done(err);
+                newBillId = res.body.id;
+                res.should.have.status(201);
+                res.body.should.be.a("object");
+                done();
+            });
+    });
 
 
         it("should return an error if first name  is not present while creating a user", function (done) {
@@ -114,8 +161,7 @@ describe("put/v1/user/self", function () {
                 var server = app.listen();
                 request(server)
                     .get("/v1/bills")
-                    .auth('test1_new@gmail.com', 'Ananya@18')
-
+                    .auth('unitTesting@husky.neu.edu', 'AnaEfron@1713!')
                     .end(function (err, res) {
                         if (err) done(err);
                         res.should.have.status(200);
@@ -130,33 +176,29 @@ describe("put/v1/user/self", function () {
     });
 describe("put/v1/bill/:id", function () {
 
-    it("should not create the bill by diffrent user", function (done) {
+    it("should update a bill of the given id when user is authorized ", function (done) {
+        var server = app.listen();
         var payload = {
-            vendor: "NEU University",
-            bill_date: "2020-01-06",
-            due_date: "2020-01-12",
-            amount_due: 4.51,
-            categories: [
+            "vendor": "Updated Bill",
+            "bill_date": "2020-01-28",
+            "due_date": "2020-01-31",
+            "amount_due": 500.22,
+            "categories": [
               "college",
               "tuition",
-            
               "spring2020"
             ],
-            paymentStatus: "paid"
-        };
-        var server = app.listen();
+            "paymentStatus": "paid"
+          };
         request(server)
-        .put(`/v1/bill/:id`)
-            .auth('test1_new@gmail.com','Ananya@18')
-            
+            .put(`/v1/bill/${newBillId}`)
+            .auth('unitTesting@husky.neu.edu', 'AnaEfron@1713!')
             .send(payload)
             .end(function (err, res) {
                 if (err) done(err);
-                newCreateBill = res.body.id;
-                res.should.have.status(400);
+                res.should.have.status(200);
                 res.body.should.be.a("object");
                 done();
-
             });
     });
 

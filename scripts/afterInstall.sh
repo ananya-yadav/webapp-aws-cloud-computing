060db21fad0678c99eeacb7549962ff821bf88f7
Ananya Yadav <yadav.ana@husky.neu.edu>
2020-03-18 07:05:24 -0400

    Tetsing cloud watch agent

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -5,5 +5,5 @@ sudo npm install
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
     -a fetch-config \
     -m ec2 \
-    -c file:cloudWatchAgent.json \
+    -c file:/cloudWatchAgent.json \
     -s
